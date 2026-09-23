0c26551387aad932a0ae70e6d86a637c757b9926
tristan-gscn <tristan.gscn@proton.me>
2026-09-24 00:49:06 +0200

    chore: configure Hardhat with the subject directory layout

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  paths: {
+    sources: "./code",
+    tests: "./test",
+  }
+};
+
+export default config;
